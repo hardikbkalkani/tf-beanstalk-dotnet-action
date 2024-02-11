b006df2237a1a66060433541b1ed250df82c5278
Hardik Kalkani <hardikkalkani222@gmail.com>
2024-02-11 18:08:35 +0530

    Final

--- infrastructure/vars.tf
+++ infrastructure/vars.tf
@@ -9,11 +9,20 @@ variable "beanstalkappenv" {
 }
 variable "solution_stack_name" {
   type = string
+  default = "64bit Amazon Linux 2023 v3.0.3 running .NET 6"
 }
 variable "tier" {
   type = string
+  default = "WebServer"
+
 }
 
-variable "vpc_id" {}
-variable "public_subnets" {}
-variable "elb_public_subnets" {}
+variable "vpc_id" {
+  default = "vpc-06c36b039ef247e27"
+}
+variable "public_subnets" {
+  default = ["subnet-05316993df815da53,subnet-0caaa77458d02d4e0"]
+}
+variable "elb_public_subnets" {
+  default = "subnet-05316993df815da53,subnet-0caaa77458d02d4e0"
+}
