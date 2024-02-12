014eff65ec9f7931638237e0609baf0f84a21539
test <hardikkalkani22@gmail.com>
2024-02-12 15:44:34 +0530

    1st

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,34 +1,9 @@
-// var builder = WebApplication.CreateBuilder(args);
-
-// // Add services to the container.
-
-// builder.Services.AddControllers();
-// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
-// var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
-// app.MapGet("/", () => "Hello World!");
-
-// app.MapControllers();
-
-// app.Run();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -38,23 +13,48 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
-// Map the root path ("/") to the Swagger UI
-app.MapGet("/", context =>
-{
-    // Redirect to the Swagger UI path
-    context.Response.Redirect("/swagger/index.html");
-    return Task.CompletedTask;
-});
+app.MapGet("/", () => "Hello World!");
 
 app.MapControllers();
 
 app.Run();
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// builder.Services.AddControllers();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI(c =>
+//     {
+//         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+//     });
+// }
+
+// app.UseHttpsRedirection();
+// app.UseAuthorization();
+
+// // Map the root path ("/") to the Swagger UI
+// app.MapGet("/", context =>
+// {
+//     // Redirect to the Swagger UI path
+//     context.Response.Redirect("/swagger/index.html");
+//     return Task.CompletedTask;
+// });
+
+// app.MapControllers();
+
+// app.Run();
